991adb5daaebdd92b24fbf00f66ce5d7ea6db16f
eduardohk <eduardohk@users.noreply.github.com>
2016-06-14 14:43:28 -0300

    Upload script para criação da tabela de usuário.

---
+++ script/script_table_usuario.sql
@@ -0,0 +1,12 @@
+﻿--DROP TABLE Usuario;
+--SELECT * FROM Usuario;
+CREATE TABLE Usuario(
+	uNome 		varchar(30) 	not null,
+	uSenha 		varchar(30) 	not null,
+	uCelular 	varchar(20) 	not null,
+	constraint uPK 
+		primary key(uNome)
+);
+
+	
+	
